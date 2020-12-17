1e310e5499b2be957b4874d7c463cd04b55a648d
sanjiv sahayam <sanjsmailbox@gmail.com>
2020-12-18 08:44:52 +1100

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# Slate DB
+
+Database backend for [Slate](https://github.com/ssanj/slate)
---
+++ seed/seed.sql
@@ -0,0 +1,1 @@
+INSERT INTO SCRIB (MESSAGE, CREATED_AT, UPDATED_AT) VALUES (replace('# Welcome to Scrib\nThis is your first note', '\n', char(10)), '2020-01-01T00:00:00.000', '2020-01-01T00:00:00.000');
